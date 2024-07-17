8fc0802b8a18f6428cd0375b14cc0c12cb36086b
Giulio Seregni <giulio.seregni@pingendo.com>
2024-07-17 14:42:49 +0200

    m

--- src/app/services/ws.service.ts
+++ src/app/services/ws.service.ts
@@ -18,36 +18,22 @@ export class WSService {
     async start() {
 
         const config = {
-        iceServers: [
-            { 
-            "urls": "stun:stun.l.google.com:19302",
-            }
-        ]
+            iceServers: [
+                { 
+                    "urls": "stun:stun.l.google.com:19302",
+                }
+            ]
         };
 
         this.socket = await io("http://localhost:1337");
         
-        console.log("STARTTTTT ")
-        
         this.socket.on("tree_refresh", (message) => {
-            // console.log("read1", message)
-            // var vs = JSON.parse(message)
-            // console.log(message)
+            console.log("tree_refresh", message)
             this.tree = message
-           /*  vs.forEach(v => {
-                let h = this.getHandle(v.name)
-                h.plc = v.plc
-                h.value = v.value
-                this.change$.next(h)
-            }) */
-            // console.log("read2", this.variables)
         })
 
         this.socket.on("connect", () => {
             this.socket?.emit("helo")
-           /*  this.socket.emit("plc_subscriber",this.socket.id, "RULLIERA")
-            this.socket.emit("plc_subscriber",this.socket.id, "ZONA3")
-            this.socket.emit("plc_subscriber",this.socket.id, "ASPIRAZIONE") */
         });
 
     }
